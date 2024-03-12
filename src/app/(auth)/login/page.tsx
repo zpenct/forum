26a4c5831b09b70756b725a120fd5de47af8e680
Fayzul <fayzulhaq0708@gmail.com>
2024-03-12 14:27:26 +0800

    chore: change bg color

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,7 +2,7 @@ import FormLogin from "./Form";
 import Link from "next/link";
 export default function page() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-slate-200">
+    <div className="flex min-h-screen flex-col items-center justify-center">
       <div className="px-5 md:w-1/2 lg:w-1/3 w-full md:px-0">
         <h1 className="text-3xl font-bold">Login</h1>
         <p className="mb-3 text-slate-600">
