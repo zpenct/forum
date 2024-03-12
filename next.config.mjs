3235c0da2cefc6c6e629218b30c9e6c57a5005df
Fayzul <fayzulhaq0708@gmail.com>
2024-03-12 11:01:22 +0800

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        dangerouslyAllowSVG: true,
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'ui-avatars.com',
+            port: '',
+            pathname: '/api/**',
+          },
+        ],
+      },
+};
 
 export default nextConfig;
+// ui-avatars.com
