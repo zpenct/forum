609c2e643552b444687cabe97028fc0cb199c8e7
Fayzul <fayzulhaq0708@gmail.com>
2024-03-17 15:06:08 +0800

    fix: ui leaderbordpage

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,8 +1,8 @@
-import UserTable from "@/components/leaderboard/UserTable"
+import UserTable from "@/components/leaderboard/UserTable";
 export default function page() {
   return (
-    <div className="p-3 md:mt-10 md:pl-10 md:max-w-3xl">
-        <UserTable />
+    <div className="p-3 md:mt-10 md:pl-10 md:max-w-3xl pb-20">
+      <UserTable />
     </div>
-  )
+  );
 }
