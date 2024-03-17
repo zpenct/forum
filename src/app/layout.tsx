daa9120f32260f617f49d9321a5e6088280a7e7b
Fayzul <fayzulhaq0708@gmail.com>
2024-03-17 15:07:03 +0800

    chore: remove use client and add session Provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
-"use client"
-
 import { Inter } from "next/font/google";
 import Sidebar from "@/components/Sidebar/Sidebar";
 import StoreProvider from "@/components/StoreProvider";
+import SessionProvider from "./SessionProvider";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,12 +16,14 @@ export default function RootLayout({
       <body
         className={`${inter.className} grid md:grid-cols-12 gap-2 bg-white`}
       >
-        <StoreProvider>
-          <div className="md:col-span-3 sticky">
-            <Sidebar />
-          </div>
-          <div className="md:col-span-9">{children}</div>
-        </StoreProvider>
+        <SessionProvider>
+          <StoreProvider>
+            <div className="md:col-span-3 sticky">
+              <Sidebar />
+            </div>
+            <div className="md:col-span-9">{children}</div>
+          </StoreProvider>
+        </SessionProvider>
       </body>
     </html>
   );
